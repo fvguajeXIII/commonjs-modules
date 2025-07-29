ee487bd975d09f0a22b9b838bfc4c4ec7e7da5da
fvguajexiii <f.vellucci13@outlook.it>
2025-07-29 13:52:17 +0200

    Creata la funzione che mi restituisce un oggetto con le due proprietà

--- people.js
+++ people.js
@@ -3,3 +3,14 @@ const importNames = require('./names.js')
 
 //Importiamo la funzione da hobbies.js
 const importHobbies = require('./hobbies.js')
+
+//Creo la funzione che mi restituisce un oggetto con le due proprietà
+const createPerson = () => {
+  const fullName = importNames("Fabio", "Vellucci");
+  const hobbies = importHobbies("Calcio", "Formula 1", "Marvel");
+
+  return {
+    fullName: fullName,
+    hobbies: hobbies
+  };
+};
