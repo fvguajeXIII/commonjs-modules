e7809f25bbaa5987967f68e183180372fd9a4c8f
fvguajexiii <f.vellucci13@outlook.it>
2025-07-29 13:43:54 +0200

    Modulo hobbies

---
+++ hobbies.js
@@ -0,0 +1,10 @@
+//Funzione che accetta hobbyOne, hobbyTwo, Hobbythree e restituisce un array
+const createHobbies = (hobbyOne, hobbyTwo, hobbyThree) => {
+  return [
+    hobbyOne,
+    hobbyTwo,
+    hobbyThree
+  ];
+};
+//Esporto la funzione
+module.exports = createHobbies;
